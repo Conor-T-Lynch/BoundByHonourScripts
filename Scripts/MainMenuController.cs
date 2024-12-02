e184a119a0cba8e668ed34ff70abd74a2582e2ff
Conor-T-Lynch <93265951+Conor-T-Lynch@users.noreply.github.com>
2024-12-02 15:33:56 +0000

    Add files via upload

--- Scripts/MainMenuController.cs
+++ Scripts/MainMenuController.cs
@@ -1,25 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+//@Reference https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.LoadScene.html
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Method to start a new game
+    //Method to start a new game
     public void StartNewGame()
     {
-        // Load the first level or scene of your game
+        //Load the first level or scene of your game
         SceneManager.LoadScene("SampleScene");
     }
 
-    // Method to load an existing game
+    //Method to load an existing game
     public void LoadGame()
     {
         
         SceneManager.LoadScene("GameScene");
     }
 
-    // Method to quit the game
+    //Method to quit the game
     public void QuitGame()
     {
         Application.Quit();
